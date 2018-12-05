831652f1c8b05711b0c55eb425eecc28a2ad11d8
Laura Shamus <lkshamus@gmail.com>
2018-12-04 17:07:48 -0700

    Add basic server functionality

--- server.js
+++ server.js
@@ -8,3 +8,13 @@ const database = require('knex')(config)
 app.use( bodyParser.json() );
 
 app.use(express.static('public'));
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (request, response) => {
+  response.send('hello world');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`App is running on localhost:${app.get('port')}.`);
+});
