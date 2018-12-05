caadcb8af3dcdd562ada244fc4446b51c071f2a5
Laura Shamus <lkshamus@gmail.com>
2018-12-04 18:21:43 -0700

    Add separate endpoints for artist and album

--- server.js
+++ server.js
@@ -9,12 +9,32 @@ app.use( bodyParser.json() );
 
 app.use(express.static('public'));
 
+app.locals.title = 'Album Finder'
+
 app.set('port', process.env.PORT || 3000);
+
+app.get('/api/v1/artists', (request, response) => {
+  database('artists').select()
+    .then(artists => {
+      response.status(200).json(artists)
+    })
+    .catch(error => {
+      response.status(500).json({ error: error.message })
+    })
+})
+
+app.get('/api/v1/artists/:id', (request, response) => {
+  const { id } = request.params
+
+  database('artists').where('id', id).select()
+    .then(artist => response.status(200).json(artist))
+    .catch(error => console.log(`Error fetching artist: ${error.message}`))
+})
 
 app.get('/', (request, response) => {
   response.send('hello world');
 });
 
 app.listen(app.get('port'), () => {
-  console.log(`App is running on localhost:${app.get('port')}.`);
+  console.log(`${app.locals.title} is running on localhost:${app.get('port')}.`);
 });
