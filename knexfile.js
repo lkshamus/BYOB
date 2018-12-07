23cfef3a314ff86ffab6a2000366da363ee0b5c9
Laura Shamus <lkshamus@gmail.com>
2018-12-07 13:03:34 -0700

    Rearrange server file by end points

--- knexfile.js
+++ knexfile.js
@@ -22,11 +22,11 @@ module.exports = {
     }
   },
   production: {
-  client: 'pg',
-  connection: process.env.DATABASE_URL + `?ssl=true`,
-  migrations: {
-    directory: './db/migrations'
-  },
-  useNullAsDefault: true
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
 }
 }
