15eb0199567047456c5045ee5be86ce46255c456
Drake Bott <bott.da@gmail.com>
2018-12-11 14:12:10 -0700

    Remove bodyParser middleware in favor of built in express parser

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const environment = process.env.NODE_ENV || 'development'
 const config = require('./knexfile')[environment]
 const database = require('knex')(config)
@@ -29,7 +28,7 @@ function checkParams(request, response, next) {
   }
 }
 
-app.use( bodyParser.json() );
+app.use( express.json() );
 
 app.use(express.static('public'));
 
